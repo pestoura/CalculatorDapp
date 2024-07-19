a50225dad4b77886603fc01ca054e16fc2fe7970
Pedro Estoura <pedro.estoura@gmail.com>
2024-07-19 21:11:50 +0100

    Update calculator.sol

--- calculator.sol
+++ calculator.sol
@@ -11,24 +11,40 @@ contract Calculator {
     // Declares a state variable of type uint256 (unsigned integer of 256 bits) named 'result'
     // and initializes it to 0. This variable stores the result of the operations.
 
+    /**
+     * @dev Adds a number to the result.
+     * @param num The number to add.
+     */
     function add(uint256 num) public {
         // Defines a public function named 'add' that takes a parameter 'num' of type uint256.
         // This function adds the value of 'num' to the 'result' variable.
         result += num;
     }
 
+    /**
+     * @dev Subtracts a number from the result.
+     * @param num The number to subtract.
+     */
     function subtract(uint256 num) public {
         // Defines a public function named 'subtract' that takes a parameter 'num' of type uint256.
         // This function subtracts the value of 'num' from the 'result' variable.
         result -= num;
     }
 
+    /**
+     * @dev Multiplies the result by a number.
+     * @param num The number to multiply with.
+     */
     function multiply(uint256 num) public {
         // Defines a public function named 'multiply' that takes a parameter 'num' of type uint256.
         // This function multiplies the value of 'result' by the value of 'num'.
         result *= num;
     }
 
+    /**
+     * @dev Returns the current result.
+     * @return The current value of the 'result' variable.
+     */
     function get() public view returns (uint256) {
         // Defines a public view function named 'get' that takes no parameters.
         // This function returns the current value of the 'result' variable.
